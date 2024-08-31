bfcd2f58c8d4471c84036dbbddfc9e2550f8a5ba
GaneshKumar586 <kganeshkumar58627@gmail.com>
2024-08-31 17:00:18 +0530

    added .env file to hide confidential data

--- fashionWardrobe/src/main/java/com/gana/fashionWardrobe/FashionWardrobeApplication.java
+++ fashionWardrobe/src/main/java/com/gana/fashionWardrobe/FashionWardrobeApplication.java
@@ -3,11 +3,15 @@ package com.gana.fashionWardrobe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @EntityScan(basePackages = "com.gana.model")
 @SpringBootApplication
 public class FashionWardrobeApplication {
-
+	Dotenv dotenv = Dotenv.load();
+	String mysqlHost = dotenv.get("MYSQL_HOST");
+	String dbUsername = dotenv.get("DB_USERNAME");
+	String dbPassword = dotenv.get("DB_PASSWORD");
 	public static void main(String[] args) {
 		SpringApplication.run(FashionWardrobeApplication.class, args);
 	}
